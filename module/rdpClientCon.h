f711ef9fcdea14b471abb132a2ce55d360b7dad4
Christopher Pitstick <cmp@pitstick.net>
2021-02-25 01:02:34 -0500

    Improve stability of xorgxrdp when resolution changes during a session ("dynamic resolution")
    During dynamic resize, it's possible for the capture timer to trigger while the screen's memory buffer is not in a proper state for the given codec used.
    
    This patches the issue so that the xorgxrdp driver won't crash, but I could envision further enhancements to the _rdpClientCon data structure that make this easier to access.

--- module/rdpClientCon.h
+++ module/rdpClientCon.h
@@ -49,6 +49,14 @@ struct rdpup_os_bitmap
     int stamp;
 };
 
+enum shared_memory_status {
+    SMS_UNINITIALIZED = 0,
+    SMS_RESIZING,
+    SMS_ACTIVE,
+    SMS_RFX_ACTIVE,
+    SMS_H264_ACTIVE
+};
+
 /* one of these for each client */
 struct _rdpClientCon
 {
@@ -103,6 +111,7 @@ struct _rdpClientCon
     RegionPtr shmRegion;
     int rect_id;
     int rect_id_ack;
+    enum shared_memory_status shmemstatus;
 
     OsTimerPtr updateTimer;
     CARD32 lastUpdateTime; /* millisecond timestamp */
